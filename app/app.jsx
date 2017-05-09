be931d9caa109966aef04298a69992690c2f06e9
d33pak-singh <singh.deepakk1992@gmail.com>
2017-05-09 23:22:35 +0530

    Styles addded!

--- app/app.jsx
+++ app/app.jsx
@@ -1,15 +1,16 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
-
 var Main = require('Main');
-
 var Weather = require('Weather');
-
 var About = require('About');
-
 var Example = require('Example');
+
+
+// Load foundation
+require('style!css!foundation-sites/dist/foundation.min.css');
+$(document).foundation();
+
 
 ReactDOM.render(
   <Router history={hashHistory}>
